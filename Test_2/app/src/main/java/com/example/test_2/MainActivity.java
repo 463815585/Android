d2600f4f070af989b7583a4af33c31cbe00049a5
463815585 <463815585@qq.com>
2022-10-02 00:04:44 +0800

    添加实验二源码

---
+++ Test_2/app/src/main/java/com/example/test_2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.test_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.test_1);
+        setContentView(R.layout.test_3);
+    }
+}
